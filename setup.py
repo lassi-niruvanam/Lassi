537248baf406aded645d33340fecd20bdf57c65d
julienmalard <julien.malard@mail.mcgill.ca>
2020-08-14 09:32:00 -0400

    0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lassi',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(where="மூலம்"),
     package_dir={"": "மூலம்"},
     url='https://lassi.readthedocs.io',
